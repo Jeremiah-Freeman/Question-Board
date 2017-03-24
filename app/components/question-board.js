4cd6a2f3c608ff896057d7791e2968c76eb534ce
Jay Freeman <jaythinkshappiness@gmail.com>
2017-03-24 09:55:57 -0700

    add save functionality to question-board.js with edit to it's hbs page to make sure the variable name match

--- app/components/question-board.js
+++ app/components/question-board.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    saveQuestion1() {
+      var params = {
+        question: this.get('question'),
+        author: this.get('author'),
+      };
+      this.sendAction('saveQuestion2', params);
+    }
+  }
 });
